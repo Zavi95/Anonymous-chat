2a671136842e6473af60dc983b9ecde01ce63043
Zavi95 <zavian771@gmail.com>
2022-10-17 19:40:39 -0400

    Added upload photo function

--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import Chatbox from "./Chatbox";
+import Upload from "./Upload";
 import "./Input.css";
+
+//How to upload local images onClick on ReactJS?
 
 function Input() {
   const [input, setInput] = useState("");
   const [text, setText] = useState([]);
+  const [image, setImage] = useState([]);
 
   const BtnClick = (e) => {
     setText(text.concat(<Chatbox message={input} key={text.length} />));
     setInput("");
     e.prevent.default();
+  };
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const AddImage = () => {
+    setImage(image.concat(<Upload key={image.lenght} />));
   };
 
   return (
@@ -20,16 +32,20 @@ function Input() {
           type="text"
           placeholder="Post here"
           value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
+          onChange={handleChange}
         />
         <button type="button" className="input_button" onClick={BtnClick}>
           Post
         </button>
+        <button type="button" className="input_button" onClick={AddImage}>
+          Image
+        </button>
       </form>
 
       {text}
+      {image}
+
+      <br />
     </div>
   );
 }
